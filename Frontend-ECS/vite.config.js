b44eba215b2190bc07c289e8176d3c7f2103cf8a
piyush186sharma <piyushsharmaspsghy@gmail.com>
2025-11-16 18:53:31 +0530

    frontend push

--- Frontend-ECS/vite.config.js
+++ Frontend-ECS/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:7000',
+        target: 'https://ecs-gdof.onrender.com',
+        changeOrigin: true,
         secure: false,
       }
     },
